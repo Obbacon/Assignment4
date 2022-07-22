a951a55c84f1e1f84d77ee3ec3e4493276fe556a
Obbacon <rayx70@live.ca>
2022-07-22 11:42:00 -0400

    Added stuff

--- 191867440_allocation.c
+++ 191867440_allocation.c
@@ -1,3 +1,6 @@
+// Author: Stephen Morris - 191867440
+// Github Link - 
+
 #include<stdlib.h>
 #include<string.h>
 #include<stdio.h>
@@ -5,16 +8,42 @@
 void main()
 
 {
-    int bSize[15], pSize[15], throws[15], allocation[15];
-    int i, j;
+    int bSize[15], pSize[15], throws[15]; 
+    int alloc[15];
+    int i = 0;
+    int j = 0;
     int processNumber, blockNumber;
 
-    for(i = 0; i < 10; i++) {
+    printf("Setup: ");
+    for(i; i < 15; i++) {
+        alloc[i] = -1;
         throws[i] = 0;
-        allocation[i] = -1;
     }
     printf("Allocation amount:  ");
     scanf("%d", &blockNumber);
     printf("\nEnter size: ");
 
+    for(i; i < blockNumber; i++){
+        scanf("%d", bSize[i]);
+    } 
+
+    printf("Get processes ");
+    scanf("%d", &processNumber);
+
+    for (i; i < processNumber; i++){
+        scanf("%d", &pSize[i]);
+    }
+
+    for(i; i< processNumber; i++){
+        for(j; j < blockNumber; j++){
+            if(throws[j] == 0 && bSize[j] >= pSize[i]){
+                alloc[j] = i;
+                break;
+            }
+        }
+    }
+    for (i; i < blockNumber; i++){
+        printf("\n%d\t", i + 1);
+    }
+    
 }
