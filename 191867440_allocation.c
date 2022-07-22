d35551301bd001ec45238a93ca328279000a22e4
Obbacon <rayx70@live.ca>
2022-07-22 10:40:45 -0400

    Added Stuff

---
+++ 191867440_allocation.c
@@ -0,0 +1,20 @@
+#include<stdlib.h>
+#include<string.h>
+#include<stdio.h>
+
+void main()
+
+{
+    int bSize[15], pSize[15], throws[15], allocation[15];
+    int i, j;
+    int processNumber, blockNumber;
+
+    for(i = 0; i < 10; i++) {
+        throws[i] = 0;
+        allocation[i] = -1;
+    }
+    printf("Allocation amount:  ");
+    scanf("%d", &blockNumber);
+    printf("\nEnter size: ");
+
+}
